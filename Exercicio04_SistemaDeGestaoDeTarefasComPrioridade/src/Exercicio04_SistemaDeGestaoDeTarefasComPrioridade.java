dec69c330294ef744788cc87811b1b10002b9756
saulo.rodrigues <saulohrodrigues@gmail.com>
2025-07-10 10:14:50 -0300

    Alterando o exercicio 04, retirando a redundância apontada no feedback.

--- Exercicio04_SistemaDeGestaoDeTarefasComPrioridade/src/Exercicio04_SistemaDeGestaoDeTarefasComPrioridade.java
+++ Exercicio04_SistemaDeGestaoDeTarefasComPrioridade/src/Exercicio04_SistemaDeGestaoDeTarefasComPrioridade.java
@@ -6,26 +6,35 @@ public class Exercicio04_SistemaDeGestaoDeTarefasComPrioridade {
     public static void main(String[] args) {
 
         Tarefa lavarLouca = new Tarefa("Lavar louça", "Lavar a louça do almoço", Prioridade.BAIXA);
-        ListaDeTarefas  tarefas = new ListaDeTarefas(lavarLouca);
-        tarefas.adicionarTarefa("Lavar o Carro","Lavar o Carro",Prioridade.BAIXA);
+        Tarefa lavarCarro = new Tarefa ("Lavar o Carro","Lavar o Carro",Prioridade.BAIXA);
+        Tarefa limparCasa = new Tarefa("Limpar a casa","todo sabado deve limpar a casa",Prioridade.MEDIA);
+        Tarefa entregarExercicios= new Tarefa("Etregar os exercicios","Etregar os exercicios para Samara",Prioridade.ALTA);
+        Tarefa darBanhoNoCachorro = new Tarefa("Dar banho no cachorro","Dar banho no cachorro",Prioridade.BAIXA);
 
-        tarefas.adicionarTarefa("Limpar a casa","todo sabado deve limpar a casa",Prioridade.MEDIA);
-        tarefas.adicionarTarefa("Etregar os exercicios","Etregar os exercicios para Samara",Prioridade.ALTA);
-        tarefas.adicionarTarefa("Limpar a casa","todo sabado deve limpar a casa",Prioridade.MEDIA);
-        tarefas.adicionarTarefa("Dar banho no cachorro","Dar banho no cachorro",Prioridade.BAIXA);
+        ListaDeTarefas  tarefas = new ListaDeTarefas();
+        tarefas.adicionarTarefa(lavarLouca);
+        tarefas.adicionarTarefa(lavarCarro);
+        tarefas.adicionarTarefa(limparCasa);
+        tarefas.adicionarTarefa(entregarExercicios);
+        tarefas.adicionarTarefa(darBanhoNoCachorro);
+
 
 //        tarefas.exibirListaDetarefas();
 
+        System.out.println("Tarefas por prioridade:");
+        for (Tarefa t: tarefas.ListarTrarefasPorPrioridades()) {
+            System.out.println(t);
+        }
 //        System.out.println("Lista ordenada");
 //        for(Tarefa i: tarefas.ListarTrarefasPorPrioridades()){
 //            System.out.println(i);
 //        }
 
-        lavarLouca.concluir();
-        System.out.println("Listar Pendentes");
-        for (Tarefa t: tarefas.listarTarefasPendentes()){
-            System.out.println(t);
-            System.out.println();
-        }
+//        lavarLouca.concluir();
+//        System.out.println("Listar Pendentes");
+//        for (Tarefa t: tarefas.listarTarefasPendentes()){
+//            System.out.println(t);
+//            System.out.println();
+//        }
     }
 }
